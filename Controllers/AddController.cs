c842aebb2aa954f3c181775f7f34c25d77edcf21
Kaleween <kaleween@gmail.com>
2020-07-24 07:56:57 +0100

    Initial commit

---
+++ Controllers/AddController.cs
@@ -0,0 +1,17 @@
+﻿using Angband.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Angband.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddController : ControllerBase
+    {
+        [HttpGet]
+        public long Get(int x, int y)
+        {
+            var addition = new Addition(x, y);
+            return addition.Resultat;
+        }
+    }
+}
---
+++ Controllers/SumController.cs
@@ -0,0 +1,17 @@
+﻿using Angband.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Angband.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SumController : ControllerBase
+    {
+        [HttpGet]
+        public long Get([FromQuery] int[] n)
+        {
+            var somme = new Somme(n);
+            return somme.Resultat;
+        }
+    }
+}
